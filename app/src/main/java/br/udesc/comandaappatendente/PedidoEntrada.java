e03adab43e0e64ea4c8d4bb204618a7aa2758ded
Fabio-Frare <nabata99@hotmail.com>
2019-10-12 21:12:54 -0300

    commit inicial

---
+++ app/src/main/java/br/udesc/comandaappatendente/PedidoEntrada.java
@@ -0,0 +1,16 @@
+package br.udesc.comandaappatendente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PedidoEntrada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedido_entrada);
+    }
+
+
+}
